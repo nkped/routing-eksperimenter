e5bf706d5c3534aba6e89c8a60fec8f392dda041
nkped <nikrpedev@gmail.com>
2022-05-07 12:45:14 +0200

    Initial commit

---
+++ src/routes/home.jsx
@@ -0,0 +1,9 @@
+function Home() {
+  return (
+    <main>
+      <h2>Du er på Home!</h2>
+    </main>
+  );
+}
+
+export default Home;
---
+++ src/routes/om.jsx
@@ -0,0 +1,9 @@
+function Om() {
+  return (
+    <main>
+      <h2>Du er på Om!</h2>
+    </main>
+  );
+}
+
+export default Om;
